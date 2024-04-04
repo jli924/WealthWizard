2eee508ca593673d555c131798d9abefa934bb79
jli924 <jjamiel1890@gmail.com>
2024-04-04 09:53:40 -0400

    Edited docker-compose.yml

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -13,7 +13,7 @@ customers = Blueprint('customers', __name__)
 @customers.route('/customers', methods=['GET'])
 def get_customers():
     cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
+    cursor.execute('select id, company, last_name,\
         first_name, job_title, business_phone from customers')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
