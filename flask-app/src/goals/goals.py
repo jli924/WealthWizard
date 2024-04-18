1df71401ead7c89834098ac77b355e33b1b5f5c9
Sarah LaCroix <lacroix.sa@northeastern.edu>
2024-04-17 20:57:08 -0400

    added new goals route for transactions towards goals

--- flask-app/src/goals/goals.py
+++ flask-app/src/goals/goals.py
@@ -123,3 +123,17 @@ def get_goals_accountid(Account_id):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+@goals.route('/goals-tr/<Account_id>', methods=['GET'])
+def get_goals_accountid_transac(Account_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM Goals g JOIN Transactions tr ON g.Goal_id = tr.Goal_id WHERE tr.Account_id = {0}'.format(Account_id))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
